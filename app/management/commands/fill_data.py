eb766fe4a276324fbfe9cc889b27f999c040866a
pak sem <p4ck.sem@yandex.ru>
2025-05-15 22:52:46 +0300

    HW3:connect to PostgreSQL, add models in project, fill data help for fill_data command

--- app/management/commands/fill_data.py
+++ app/management/commands/fill_data.py
@@ -4,9 +4,6 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from faker import Faker
 from app.models import Profile, Question, Answer, Tag, QuestionLike, AnswerLike
-from django.core.files import File
-from io import BytesIO
-from PIL import Image
 
 
 class Command(BaseCommand):
@@ -15,37 +12,26 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('ratio', type=int, help='The ratio multiplier for data generation')
 
-    def create_test_image(self):
-        image = Image.open("sam.jpg")
-        image_io = BytesIO()
-        image.save(image_io, format='JPEG')
-        return File(image_io, name='test.jpg')
-
     def handle(self, *args, **options):
         ratio = options['ratio']
         fake = Faker()
 
         self.stdout.write(f"Starting database population with ratio {ratio}...")
 
-        # Create Users and Profiles - теперь создаем последовательно
         self.stdout.write(f"Creating {ratio} users and profiles...")
         profiles = []
         for i in range(ratio):
-            # Создаем пользователя
             username = fake.unique.user_name()
             email = fake.email()
             password = fake.password()
             user = User.objects.create(username=username, email=email, password=password)
 
-            # Создаем профиль с аватаркой
             profile = Profile.objects.create(user_id=user)
-            profile.avatar.save('avatar.jpg', self.create_test_image())
             profiles.append(profile)
 
             if i % 100 == 0:
                 self.stdout.write(f"Created {i}/{ratio} users and profiles...")
 
-        # Create Tags
         self.stdout.write(f"Creating {ratio} tags...")
         tags = []
         for i in range(ratio):
@@ -56,7 +42,6 @@ class Command(BaseCommand):
         Tag.objects.bulk_create(tags, batch_size=1000)
         tags = list(Tag.objects.all())
 
-        # Create Questions
         self.stdout.write(f"Creating {ratio * 10} questions...")
         questions = []
         for i in range(ratio * 10):
@@ -70,7 +55,6 @@ class Command(BaseCommand):
         Question.objects.bulk_create(questions, batch_size=5000)
         questions = list(Question.objects.all())
 
-        # Create Answers
         self.stdout.write(f"Creating {ratio * 100} answers...")
         answers = []
         for i in range(ratio * 100):
@@ -85,7 +69,6 @@ class Command(BaseCommand):
         Answer.objects.bulk_create(answers, batch_size=10000)
         answers = list(Answer.objects.all())
 
-        # Create Question Likes
         self.stdout.write(f"Creating {ratio * 100} question likes...")
         question_likes = []
         existing_question_likes = set()
@@ -105,7 +88,6 @@ class Command(BaseCommand):
         if question_likes:
             QuestionLike.objects.bulk_create(question_likes, batch_size=10000)
 
-        # Create Answer Likes
         self.stdout.write(f"Creating {ratio * 100} answer likes...")
         answer_likes = []
         existing_answer_likes = set()
